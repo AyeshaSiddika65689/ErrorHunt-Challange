c92a5fee99801449f3150e26f882af5bd98d35de
AyeshaSiddika65689 <84858060+AyeshaSiddika65689@users.noreply.github.com>
2022-08-10 23:04:33 +0530

    changes

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // mongoose.connect('mongodb://localhost:27017/Library');
-const url = "mongodb://localhost:27017/Library";
+const url = "mongodb+srv://ayeshasiddika:ayesha321@cluster0.2k9xok7.mongodb.net/library";
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }); //part #2
 
 const Schema = mongoose.Schema;
